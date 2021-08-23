b487e496ddb778b012a462d1c293037d3e2d2f05
刘辉(20020013) <20020013@suning.com>
2021-08-23 15:45:29 +0800

    feat: 工作台

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Switch, Route } from "react-router-dom";
-import { Loadable } from "@kits"
+import { Loadable } from "@kits";
 
 const Home = Loadable(() => import('@pages/home'))
 
