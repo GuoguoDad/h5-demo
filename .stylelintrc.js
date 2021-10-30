452be2fc47a01b15f21684c1dc0e1bc91d82a4fe
刘辉20020013 <20020013@suning.com>
2021-10-30 15:03:10 +0800

    feat: 细节调整

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+    "stylelint-config-css-modules"
+  ],
+  "rules": {
+    "font-family-no-missing-generic-family-keyword": null,
+    "rule-empty-line-before": null,
+    "declaration-block-no-duplicate-properties": null,
+    "font-family-name-quotes": null,
+    "selector-class-pattern": null,
+    "declaration-block-no-redundant-longhand-properties": null,
+    "selector-pseudo-element-no-unknown": null
+  }
+}
