236ec897a2b15a362ac7a68f50ac6bd43c23e312
刘辉20020013 <20020013@suning.com>
2021-12-29 17:42:49 +0800

    feat: update react-router to 6

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { Loadable } from '@kits'
+import { Routes, Route } from 'react-router-dom'
+import loadable from '@loadable/component'
 
-const UserList = Loadable(() => import('@pages/user/user-list'))
-const ChartLine = Loadable(() => import('@pages/chart/chart-line'))
+const UserList = loadable(() => import('@pages/user/user-list'/* webpackChunkName: 'userList', webpackPrefetch: true */))
+const ChartLine = loadable(() => import('@pages/chart/chart-line'/* webpackChunkName: 'chartList', webpackPrefetch: true */))
 
 const App = () => {
   return (
     <Routes>
-      <Route key={'main'} path="/" element={Navigate({to: '/user/list'})} />
+      {/*<Route key={'main'} path="/" element={Navigate({to: '/user/list'})} />*/}
       <Route key={'user-list'} path="/user/list" element={<UserList/>} />
       <Route key={'chart-line'} path="/chart/line" element={<ChartLine/>} />
     </Routes>
